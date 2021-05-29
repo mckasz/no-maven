59ada726445397d8fd4cc6e94e8bf6c5108eb269
Maciej Kaszyński <m.kaszynski@ocado.com>
2021-05-30 00:35:22 +0200

    fixed scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 java -jar lib/junit-platform-console-standalone-1.7.0.jar -cp out/test/no-maven/:production/no-maven/  --scan-classpath
-jar cvfm Calculator.jar ./META-INF/MANIFEST.MF -C ./out/production/no-maven .
+rm -rf out
+mkdir out
+javac -cp "lib/*" -d out -sourcepath src src/com/mkaszynski/javatools/nomaven/*
+jar cvfm Planetary.jar ./META-INF/MANIFEST.MF -C ./out/ .
+java -jar Planetary.jar
